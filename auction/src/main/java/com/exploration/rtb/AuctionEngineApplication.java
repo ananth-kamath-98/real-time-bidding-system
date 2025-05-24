3063f0597fa2e4a819a57961e99220f16c0522ac
Ananth Kamath <ananthkamath98@gmail.com>
2025-05-24 19:48:32 -0400

    adds docker compose file; setups up microservices

---
+++ auction/src/main/java/com/exploration/rtb/AuctionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.exploration.rtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionEngineApplication.class, args);
+	}
+
+}
